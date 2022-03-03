971f3c468ee008ae41edfdf73a7390fcdbd23695
Folly-Sodoga <57939536+Jordan184@users.noreply.github.com>
2022-03-03 08:14:05 +0100

    Update routes.php

--- MySuccessStory/routes/routes.php
+++ MySuccessStory/routes/routes.php
@@ -1,19 +1,26 @@
 <?php
-// Controller class
+
+#region Use
 use Pecee\SimpleRouter\SimpleRouter;
 use MySuccessStory\Controllers\ControllerUsers;
 use MySuccessStory\controllers\ControllerNotes;
+#endregion
 
-
-SimpleRouter::get('/api/subjects', [ControllerUsers::class, 'token']);//-> returns token
-SimpleRouter::get('/api/login', [ControllerUsers::class, 'token']);
-
-SimpleRouter::get('/api/profile', [ControllerUsers::class, 'profile']);//
-
+#region Connection
+SimpleRouter::get('/api/token', [ControllerUsers::class, 'token']); // Create a token
+SimpleRouter::get('/api/login', [ControllerUsers::class, 'token' /* "login" */]); // allow the user to log in to his account
+SimpleRouter::get('/api/profile', [ControllerUsers::class, 'profile']); // shows the profile of a user
 SimpleRouter::post('/api/register', [ControllerUsers::class, 'register']);//
-//SimpleRouter::post('/api/login', [ControllerUsers::class, 'login']);//
+#endregion
 
-SimpleRouter::post('/api/notes', [ControllerNotes::class, 'create']);// create
-SimpleRouter::get('/api/notes/{subject}', [ControllerNotes::class, 'read']);//read
-SimpleRouter::patch('/api/notes/{subject}', [ControllerNotes::class, 'update']);//update
-SimpleRouter::delete('/api/notes/{subject}', [ControllerNotes::class, 'delete']);//delete
+#region CRUD Notes
+SimpleRouter::get('/api/notes', [ControllerNotes::class, 'notes']); // read all notes
+SimpleRouter::get('/api/notes/{subject}', [ControllerNotes::class, 'read']);// read a single note
+SimpleRouter::post('/api/notes', [ControllerNotes::class, 'create']);// create a note
+SimpleRouter::patch('/api/notes/{subject}', [ControllerNotes::class, 'update']);// change the value of a note
+SimpleRouter::delete('/api/notes/{subject}', [ControllerNotes::class, 'delete']);// delete a note
+#endregion
+
+#region Subjects
+SimpleRouter::get('/api/subjects', [ControllerUsers::class, 'token']); // shows all the subjects
+#endregion
