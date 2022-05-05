c8482a9088b16f4fed12d23351084206ee1bf4dc
Folly-Sodoga <ekoue-jordan.fllsd@eduge.ch>
2022-05-05 16:39:13 +0200

    Database mise à jour

--- MySuccessStory/src/db/mysuccessstory.sql
+++ MySuccessStory/src/db/mysuccessstory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mer. 13 avr. 2022 à 15:40
+-- Généré le : jeu. 05 mai 2022 à 16:36
 -- Version du serveur :  10.3.32-MariaDB
 -- Version de PHP : 7.4.24
 
@@ -30,7 +30,7 @@ DELIMITER $$
 --
 CREATE DEFINER=`mysuccessstory`@`%` PROCEDURE `updateUser` (IN `idUser` INT, IN `pwd` VARCHAR(128), IN `passwordHash` VARCHAR(128), IN `salt` INT, IN `firstName` VARCHAR(30), IN `lastName` VARCHAR(50), IN `entryYear` INT, IN `exitYear` INT)  NO SQL
 BEGIN
-			IF (pwd = "") THEN
+			IF (pwd = "") THEN 
 				UPDATE `user` SET `firstName` = firstName, `lastName` = lastName, `entryYear` = entryYear, `exitYear` = exitYear  WHERE `idUser` = idUser;
 			ELSE
 				UPDATE `user` SET `password` = pwd, `salt` = salt, `firstName` = firstName, `lastName` = lastName, `entryYear` = entryYear, `exitYear` = exitYear  WHERE `idUser` = idUser;
@@ -46,32 +46,14 @@ DELIMITER ;
 --
 
 CREATE TABLE `notes` (
-	`idNote` int(10) UNSIGNED NOT NULL,
-	`note` float UNSIGNED NOT NULL DEFAULT 4,
-	`semester` tinyint(4) UNSIGNED NOT NULL DEFAULT 1,
-	`year` int(10) UNSIGNED NOT NULL DEFAULT 1,
-	`idUser` int(10) UNSIGNED NOT NULL,
-	`idSubject` int(10) UNSIGNED NOT NULL
+  `idNote` int(10) UNSIGNED NOT NULL,
+  `note` float UNSIGNED NOT NULL DEFAULT 4,
+  `semester` tinyint(4) UNSIGNED NOT NULL DEFAULT 1,
+  `year` int(10) UNSIGNED NOT NULL DEFAULT 1,
+  `isFake` tinyint(1) NOT NULL DEFAULT 0,
+  `idUser` int(10) UNSIGNED NOT NULL,
+  `idSubject` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `notes`
---
-
-INSERT INTO `notes` (`idNote`, `note`, `semester`, `year`, `idUser`, `idSubject`) VALUES
-(159, 2, 1, 1, 1, 38),
-(160, 2, 1, 1, 1, 8),
-(161, 1, 1, 2, 1, 7),
-(162, 5, 2, 1, 9, 1),
-(163, 5, 2, 1, 9, 1),
-(164, 5, 2, 1, 9, 1),
-(165, 5, 2, 1, 9, 1),
-(166, 5, 2, 2, 9, 3),
-(167, 4, 1, 1, 1, 37),
-(168, 5, 1, 1, 1, 37),
-(169, 4, 1, 2, 1, 36),
-(170, 1, 1, 1, 1, 35),
-(171, 1, 1, 1, 1, 34);
 
 -- --------------------------------------------------------
 
@@ -80,58 +62,58 @@ INSERT INTO `notes` (`idNote`, `note`, `semester`, `year`, `idUser`, `idSubject`
 --
 
 CREATE TABLE `subjects` (
-	`idSubject` int(10) UNSIGNED NOT NULL,
-	`name` varchar(30) NOT NULL,
-	`description` varchar(200) NOT NULL,
-	`isCIE` tinyint(1) UNSIGNED NOT NULL,
-	`category` enum('CG','CFC','MATU') NOT NULL,
-	`year` int(10) UNSIGNED NOT NULL
+  `idSubject` int(10) UNSIGNED NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `isCIE` tinyint(1) UNSIGNED NOT NULL,
+  `category` enum('CG','CFC','MATU') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `subjects`
 --
 
-INSERT INTO `subjects` (`idSubject`, `name`, `description`, `isCIE`, `category`, `year`) VALUES
-(1, 'M100', 'Distinguer, préparer et évaluer des données', 0, 'CFC', 1),
-(2, 'M114', 'Mettre en oeuvre des systèmes de codification, de compression et d\'encryptage', 0, 'CFC', 1),
-(3, 'M304', 'Installer et configurer un ordinateur mono-poste', 1, 'CFC', 1),
-(4, 'M305', 'Installer, configurer et administrer un système d’exploitation', 1, 'CFC', 1),
-(5, 'M403', 'Implémenter de manière procédurale des déroulements de programme', 0, 'CFC', 1),
-(6, 'M404', 'Programmer orienté objets selon directives', 0, 'CFC', 1),
-(7, 'M101', 'Réaliser et publier un site Web', 1, 'CFC', 1),
-(8, 'M104', 'Implémenter un modèle de données', 0, 'CFC', 1),
-(9, 'M117', 'Mettre en place l’infrastructure informatique d\'une petite entreprise', 0, 'CFC', 1),
-(10, 'M123', 'Activer les services d’un serveur', 0, 'CFC', 1),
-(11, 'M307', 'Réaliser des pages Web interactives', 1, 'CFC', 1),
-(12, 'M431', 'Exécuter des mandats de manière autonome dans un environnement informatique', 0, 'CFC', 1),
-(13, 'M105', 'Traiter une base de données avec SQL', 1, 'CFC', 2),
-(14, 'M122', 'Automatiser des procédures à l’aide de scripts', 0, 'CFC', 2),
-(15, 'M426', 'Développer un logiciel avec des méthodes agiles', 0, 'CFC', 2),
-(16, 'M411', 'Développer et appliquer des structures de données et algorithmes', 0, 'CFC', 2),
-(17, 'M318', 'Analyser et programmer orienté objet avec des composants', 1, 'CFC', 2),
-(18, 'M133', 'Réaliser des applications Web en Session-Handling', 0, 'CFC', 2),
-(19, 'M151', 'Intégrer des bases de données dans des applications Web', 0, 'CFC', 2),
-(20, 'M153', 'Développer les modèles de données', 0, 'CFC', 2),
-(21, 'M213', 'Développer l\'esprit d\'équipe', 0, 'CFC', 2),
-(22, 'M335', 'Réaliser une application pour mobile', 1, 'CFC', 2),
-(23, 'EE', 'Ecole-Entreprise', 0, 'CFC', 3),
-(24, 'M121', 'Elaborer des tâches de pilotage', 0, 'CFC', 4),
-(25, 'M183', 'Implémenter la sécurité d’une application', 0, 'CFC', 4),
-(26, 'M226A-B', 'Implémenter orienté objets', 0, 'CFC', 4),
-(27, 'M242', 'Réaliser des applications pour microprocesseurs', 0, 'CFC', 4),
-(28, 'M150', 'Adapter une application de commerce électronique', 0, 'CFC', 4),
-(29, 'M151', 'Intégrer des bases de données dans des applications Web', 0, 'CFC', 4),
-(30, 'M306', 'Réaliser un petit projet informatique', 0, 'CFC', 4),
-(31, 'M326', 'Développer et implémenter orienté objets', 0, 'CFC', 4),
-(32, 'M152', 'Intégrer des contenus multimédia dans des applications Web', 0, 'CFC', 4),
-(33, 'CG-Langue & Communication', '', 0, 'CG', 0),
-(34, 'CG-Société', '', 0, 'CG', 0),
-(35, 'Anglais', '', 0, 'CG', 0),
-(36, 'Economie', '', 0, 'CG', 0),
-(37, 'Mathématique', '', 0, 'CG', 0),
-(38, 'Physique', '', 0, 'CG', 0),
-(39, 'Education Physique', '', 0, 'CG', 0);
+INSERT INTO `subjects` (`idSubject`, `name`, `description`, `isCIE`, `category`) VALUES
+(1, 'M100', 'bgrfswb', 0, 'CFC'),
+(2, 'M114', 'Mettre en oeuvre des systèmes de codification, de compression et d\'encryptage', 0, 'CFC'),
+(3, 'M304', 'Installer et configurer un ordinateur mono-poste', 1, 'CFC'),
+(4, 'M305', 'Installer, configurer et administrer un système d’exploitation', 1, 'CFC'),
+(5, 'M403', 'Implémenter de manière procédurale des déroulements de programme', 0, 'CFC'),
+(6, 'M404', 'Programmer orienté objets selon directives', 0, 'CFC'),
+(7, 'M101', 'Réaliser et publier un site Web', 1, 'CFC'),
+(8, 'M104', 'Implémenter un modèle de données', 0, 'CFC'),
+(9, 'M117', 'Mettre en place l’infrastructure informatique d\'une petite entreprise', 0, 'CFC'),
+(10, 'M123', 'Activer les services d’un serveur', 0, 'CFC'),
+(11, 'M307', 'Réaliser des pages Web interactives', 1, 'CFC'),
+(12, 'M431', 'Exécuter des mandats de manière autonome dans un environnement informatique', 0, 'CFC'),
+(13, 'M105', 'Traiter une base de données avec SQL', 1, 'CFC'),
+(14, 'M122', 'Automatiser des procédures à l’aide de scripts', 0, 'CFC'),
+(15, 'M426', 'Développer un logiciel avec des méthodes agiles', 0, 'CFC'),
+(16, 'M411', 'Développer et appliquer des structures de données et algorithmes', 0, 'CFC'),
+(17, 'M318', 'Analyser et programmer orienté objet avec des composants', 1, 'CFC'),
+(18, 'M133', 'Réaliser des applications Web en Session-Handling', 0, 'CFC'),
+(19, 'M151', 'Intégrer des bases de données dans des applications Web', 0, 'CFC'),
+(20, 'M153', 'Développer les modèles de données', 0, 'CFC'),
+(21, 'M213', 'Développer l\'esprit d\'équipe', 0, 'CFC'),
+(22, 'M335', 'Réaliser une application pour mobile', 1, 'CFC'),
+(23, 'EE', 'Ecole-Entreprise', 0, 'CFC'),
+(24, 'M121', 'Elaborer des tâches de pilotage', 0, 'CFC'),
+(25, 'M183', 'Implémenter la sécurité d’une application', 0, 'CFC'),
+(26, 'M226-A', 'Implémenter orienté objets', 0, 'CFC'),
+(27, 'M242', 'Réaliser des applications pour microprocesseurs', 0, 'CFC'),
+(28, 'M150', 'Adapter une application de commerce électronique', 0, 'CFC'),
+(29, 'M151', 'Intégrer des bases de données dans des applications Web', 0, 'CFC'),
+(30, 'M306', 'Réaliser un petit projet informatique', 0, 'CFC'),
+(31, 'M326', 'Développer et implémenter orienté objets', 0, 'CFC'),
+(32, 'M152', 'Intégrer des contenus multimédia dans des applications Web', 0, 'CFC'),
+(33, 'LangueCommunication', '', 0, 'CG'),
+(34, 'Societe', '', 0, 'CG'),
+(35, 'Anglais', '', 0, 'CG'),
+(36, 'Economie', '', 0, 'CG'),
+(37, 'Mathematique', '', 0, 'CG'),
+(38, 'Physique', '', 0, 'CG'),
+(39, 'EducationPhysique', 'desc', 0, 'CG'),
+(62, 'M226-B', 'Implémenter orienté objets', 0, 'CFC');
 
 -- --------------------------------------------------------
 
@@ -140,20 +122,10 @@ INSERT INTO `subjects` (`idSubject`, `name`, `description`, `isCIE`, `category`,
 --
 
 CREATE TABLE `token` (
-	`idUser` int(10) UNSIGNED NOT NULL,
-	`token` varchar(100) NOT NULL,
-	`expiration` int(10) UNSIGNED NOT NULL
+  `idUser` int(10) UNSIGNED NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `expiration` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `token`
---
-
-INSERT INTO `token` (`idUser`, `token`, `expiration`) VALUES
-(1, 'cVLYIsQooYtkbHboBcneMw', 1649863719),
-(9, 'cGgWva5lVA5338p9SZmyfw', 1649863708),
-(16, 'Vak54laE8t3S6Nl2-tDJyA', 1649854656),
-(17, 'GX9h9QqoxQcOP8NmRD2eAg', 1649854917);
 
 -- --------------------------------------------------------
 
@@ -162,25 +134,13 @@ INSERT INTO `token` (`idUser`, `token`, `expiration`) VALUES
 --
 
 CREATE TABLE `users` (
-	`idUser` int(10) UNSIGNED NOT NULL,
-	`email` varchar(50) NOT NULL,
-	`password` varchar(100) NOT NULL,
-	`firstName` varchar(50) NOT NULL,
-	`lastName` varchar(50) NOT NULL DEFAULT '',
-	`isTeacher` tinyint(1) UNSIGNED NOT NULL DEFAULT 0
+  `idUser` int(10) UNSIGNED NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL DEFAULT '',
+  `isTeacher` tinyint(1) UNSIGNED NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`idUser`, `email`, `password`, `firstName`, `lastName`, `isTeacher`) VALUES
-(1, 'lucasalexandre.almeidacosta@gmail.com', '$2y$10$R3gcHxfRxqnpPhjS2Da./u188Eevz12UWdjdNgeqklvlxBREHGpL.', 'Lucas', 'Almeida Costa', 0),
-(7, 'remy.bd@eduge.ch', '$2y$10$SKBOvFTKum32mUR7aTcoy.gAOL6PtSDCpFulcZ000x4VwHNhF8jEy', 'Rémy', 'Beaud', 0),
-(9, 'admin@eduge.ch', '$2y$10$p1DiFs/flFY3O7E4dAaMQeJRS173IWMgdYrEo4iu.qeJ4IV8GkvyS', 'Admin', '', 1),
-(14, 'ekoue-jordan.fllsd@eduge.ch', '$2y$10$cVOkV8Tw1dfP6/WKmJkJ2OuLzWUzwn.CDHrDqiMyczMaiZFjKZWDK', 'Jordan', 'Folly', 0),
-(16, 'jordan@eduge.ch', '$2y$10$WsS.o7V1DFY6tzkmxgRGxe.TlxF3rWMrnfGpd.fC5XTkmDsOOmadq', 'Jordan', 'Folly', 0),
-(17, 'ttt@tt.com', '$2y$10$GixLsCC2tbFoVpI3px9qh.LRR.Uja3OcYxM01QT2tS2xNoml.h8Qm', 'prenom', 'nouveau nom', 0);
 
 --
 -- Index pour les tables déchargées
@@ -190,30 +150,30 @@ INSERT INTO `users` (`idUser`, `email`, `password`, `firstName`, `lastName`, `is
 -- Index pour la table `notes`
 --
 ALTER TABLE `notes`
-	ADD PRIMARY KEY (`idNote`),
-	ADD KEY `idUser` (`idUser`),
-	ADD KEY `idSubject` (`idSubject`);
+  ADD PRIMARY KEY (`idNote`),
+  ADD KEY `idUser` (`idUser`),
+  ADD KEY `idSubject` (`idSubject`);
 
 --
 -- Index pour la table `subjects`
 --
 ALTER TABLE `subjects`
-	ADD PRIMARY KEY (`idSubject`);
+  ADD PRIMARY KEY (`idSubject`);
 
 --
 -- Index pour la table `token`
 --
 ALTER TABLE `token`
-	ADD PRIMARY KEY (`idUser`),
-	ADD UNIQUE KEY `token` (`token`),
-	ADD KEY `idUser` (`idUser`);
+  ADD PRIMARY KEY (`idUser`),
+  ADD UNIQUE KEY `token` (`token`),
+  ADD KEY `idUser` (`idUser`);
 
 --
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
-	ADD PRIMARY KEY (`idUser`),
-	ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`idUser`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -223,19 +183,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `notes`
 --
 ALTER TABLE `notes`
-	MODIFY `idNote` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=172;
+  MODIFY `idNote` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=346;
 
 --
 -- AUTO_INCREMENT pour la table `subjects`
 --
 ALTER TABLE `subjects`
-	MODIFY `idSubject` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `idSubject` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-	MODIFY `idUser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `idUser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- Contraintes pour les tables déchargées
@@ -245,13 +205,14 @@ ALTER TABLE `users`
 -- Contraintes pour la table `notes`
 --
 ALTER TABLE `notes`
-	ADD CONSTRAINT `notes_ibfk_3` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `notes_ibfk_2` FOREIGN KEY (`idSubject`) REFERENCES `subjects` (`idSubject`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notes_ibfk_3` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `token`
 --
 ALTER TABLE `token`
-	ADD CONSTRAINT `idUser` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `idUser` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
